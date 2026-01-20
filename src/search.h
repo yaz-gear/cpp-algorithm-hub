d02ddb92e2d1f2b468e855c7783933ce4846db38
yazid_ha <63234062+yaz-gear@users.noreply.github.com>
2026-01-20 18:28:12 +0000

    Create search.h

---
+++ src/search.h
@@ -0,0 +1,26 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <vector>
+
+class Search {
+public:
+    int linearSearch(const std::vector<int>& arr,int key);
+    int binarySearch(const std::vector<int>& arr,int key);
+    int ternarySearch(const std::vector<int>& arr,int key);
+};
+
+#endif
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <vector>
+
+class Search {
+public:
+    int linearSearch(const std::vector<int>& arr,int key);
+    int binarySearch(const std::vector<int>& arr,int key);
+    int ternarySearch(const std::vector<int>& arr,int key);
+};
+
+#endif
